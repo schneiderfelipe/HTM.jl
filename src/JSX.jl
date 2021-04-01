ffb1e82666b79a609e89ec2d8f79c7deab0e34bd
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-04-01 10:16:38 -0300

    Make this idea work 💡

--- src/JSX.jl
+++ src/JSX.jl
@@ -1,5 +1,117 @@
+"""
+HTML parsing on steroids!
+
+> It's basically a tree traversal!
+"""
 module JSX
 
-# Write your package code here.
+export @htm_str
+
+"""
+A node in the tree.
+"""
+struct Node
+    name::Symbol
+    attributes::Dict{Symbol,String}  # TODO: better: named tuple! Is typed, conserves order & etc.!
+    children::Vector{Union{Node,String}}
+end
+Node(name, attributes) = Node(Symbol(name), attributes, [])
+Node(name) = Node(name, Dict())
+
+Base.:(==)(a::Node, b::Node) = a.name == b.name && a.attributes == b.attributes && a.children == b.children
+
+"""
+Parse a tree by modifying the root node.
+"""
+function parse!(root, data, i=1, n=length(data))
+	if i > n || n <= 0  # Is this necessary?
+		return root, i
+	end
+
+	endtag = "</$(root.name)>"
+	if startswith(data[i:n], endtag)
+		return root, i + length(endtag)
+	elseif data[i] == '<'
+		i += 1
+		j = findnext('>', data, i) - 1
+		name = data[i:j]
+		i = j + 2
+
+		if name[end] == '/'
+			# TODO: some standard tags have no '/' but have no content by default. Test that?
+			name = rstrip(name[1:end-1])
+            hascontent = false
+        else
+            hascontent = true
+		end
+
+		attributes = Dict{Symbol,String}()
+		k = findfirst(' ', name)
+		if !isnothing(k)
+			name, rest = name[1:k-1], name[k+1:end]
+
+			for pair in split(rest)
+				key, value = split(pair, "=")
+				push!(attributes, Symbol(key) => strip(value, '"'))
+			end
+		end
+
+		child = Node(name, attributes)
+		if hascontent
+			child, i = parse!(child, data, i, n)
+		end
+
+		push!(root.children, child)
+	else
+		j = findnext('<', data, i)
+		if !isnothing(j)
+			j -=  1
+		else
+			j = n
+		end
+		text = data[i:j]
+		i += length(text)
+
+		# TODO: should we parse HTML comments?
+		text = replace(text, r"\s+" => " ")
+		if !isempty(text) && text != " "
+			push!(root.children, text)
+		end
+	end
+
+	return parse!(root, data, i, n)
+end
+
+"""
+Parse a tree.
+"""
+function parse(data)
+	root = parse!(Node(:root), data)[1]
+	if length(root.children) == 1
+		return root.children[1]
+	end
+	return root
+end
+
+"""
+Time to create a macro!
+
+We want to do the following.
+
+# Examples
+```jldoctest
+julia> htm"<h1>Hello world!</h1>"
+```
+"""
+# The real trick (that I learned by reading the code of Markdown) is to ignore
+# string interpolations completely, but consider them part of the input and
+# conserve them until the very end. Then, the macro returns not a finished
+# tree, but an expression that constructs one. The tree should allow arbitrary
+# objects in this case.
+macro htm_str(str)
+	# https://stackoverflow.com/a/39499403/4039050
+	data = esc(Meta.parse("\"$(escape_string(str))\""))
+	:(parse($data))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,54 @@
 using JSX
 using Test
 
+using JSX: Node
+
 @testset "JSX.jl" begin
-    # Write your tests here.
+    # Who doesn't love hand-made, artisanal tests?
+
+    # Some simple tests for single tags in different forms.
+    @test htm"<hr />" == Node(:hr, Dict(), [])
+    @test htm"<meta charset=\"UTF-8\" />" == Node(:meta, Dict(:charset => "UTF-8"), [])
+    @test htm"<a></a>" == Node(:a, Dict(), [])
+    @test htm"<title>JSX</title>" == Node(:title, Dict(), ["JSX"])
+    @test htm"<html lang=\"pt-BR\"></html>" == Node(:html, Dict(:lang => "pt-BR"), [])
+    @test htm"<a href=\"kitty.jpg\">A kitty!</a>" == Node(:a, Dict(:href => "kitty.jpg"), ["A kitty!"])
+    @test htm"<noscript><strong>Sorry, no JavaScript!</strong></noscript>" == Node(:noscript, Dict(), [Node(:strong, Dict(), ["Sorry, no JavaScript!"])])
+
+    # A wild lonely tag has appeared!
+    @test htm"""
+        <section>
+            <h1>Awesome title</h1>
+            <p>A <strong>bold</strong> paragraph!</p>
+            <img src="kitty.jpg" />
+        </section>
+    """ == Node(:section, Dict(), [
+        Node(:h1, Dict(), ["Awesome title"]),
+        Node(:p, Dict(), [
+            "A ",
+            Node(:strong, Dict(), ["bold"]),
+            " paragraph!",
+        ]),
+        Node(:img, Dict(:src => "kitty.jpg"), []),
+    ])
+
+    # A wild attribute has appeared!
+    @test htm"""
+        <person name="John" surname="Doe">
+            A <em>nice</em> guy
+        </person>
+    """ == Node(:person, Dict(:name => "John", :surname => "Doe"), [
+        " A ",
+        Node(:em, Dict(), ["nice"]),
+        " guy ",
+    ])
+
+    # TODO: make this more clever! htm"<country name=\"Brazil\", continent=\"South America\">" should call this function!
+    country(name, continent) = htm"""
+        <country>
+            <name>$name</name>
+            <continent>$continent</continent>
+        </country>
+    """
+    @test country("Brazil", "South America") == Node(:country, Dict(), [Node(:name, Dict(), ["Brazil"]), Node(:continent, Dict(), ["South America"])])
 end
